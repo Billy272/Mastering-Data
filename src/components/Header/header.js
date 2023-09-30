a2fbd07f9217d8caa6ca971f3aab9887314f1d08
Billy272 <billylaroi@gmail.com>
2023-09-30 14:23:30 +0300

    commit

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,4 +1,20 @@
 import React from 'react'
 import "./header.css"
 
-
+const App = () => {
+        const [count, setCount] = useState(0);
+       
+        const increment = () => {
+          setCount(prevCount => prevCount + 1);
+        };
+       
+        return (
+          <div className="App">
+            <h1>Royalty Associates</h1>
+            <h2>Count: {count}</h2>
+            <button onClick={increment}>Increment</button>
+          </div>
+        );
+       };
+       
+export default App;
