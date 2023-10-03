32f86f5dddc96c65e7f03fe6c09d1cac954eca16
Billy272 <billylaroi@gmail.com>
2023-10-03 15:23:31 +0300

    commit

--- rawyalty/src/components/App/App.js
+++ rawyalty/src/components/App/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import React from 'react';
+import Header from '../Header/Header';
+import Services from '../Services/Services';
+import About from '../About/About';
+import Contact from '../Contact/Contact';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Services />
+      <About />
+      <Contact />
     </div>
   );
 }
---
+++ rawyalty/src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Header() {
+    <head>
+        <div className="Header">
+            <h1>Rawyalty</h1>
+            <h3>Automotive Technology.</h3>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Services</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </div>
+    </head>
+}
+
+export default Header;
